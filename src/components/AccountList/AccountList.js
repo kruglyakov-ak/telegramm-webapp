8c43e06ef2a5f6f6a57ab2a06cd139c8a299eb38
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-23 17:06:03 +0300

    add accounts list

--- src/components/AccountList/AccountList.js
+++ src/components/AccountList/AccountList.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './AccountList.css';
+import React from "react";
+import "./AccountList.css";
+import { Button } from "../Button/Button";
 
+const accounts = [0, 1, 2, 3, 4, 5, 6, 7];
 function AccountList(props) {
-    return (
-        <div className="account-list">
-            Account List
-        </div>
-    );
+  return (
+    <div className="account-list">
+      <Button>Добавить аккаунт</Button>
+
+      {accounts.map((account) => (
+        <Button key={account}>Аккаунт {account}</Button>
+      ))}
+    </div>
+  );
 }
 
 export default AccountList;
