097359c9ba8a8d279258e287e7d5ac287ad6e72d
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-23 17:07:43 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useTelegram } from "./hooks/useTelegram";
-import { Button } from "./components/Button/Button";
 import AccountList from "./components/AccountList/AccountList";
 import "./App.css";
 
 function App() {
-  const { tg, onClose } = useTelegram();
+  const { tg } = useTelegram();
   useEffect(() => {
     tg.ready();
 
     console.log(tg.initData);
   }, [tg]);
 
-  const onCloseButtonHandler = () => {
-    onClose();
-  };
-
   return (
     <div className="app">
-      <Button onClick={onCloseButtonHandler}>Close</Button>
-
       <Routes>
         <Route index element={<AccountList />} />
       </Routes>
