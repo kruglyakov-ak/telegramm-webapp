de4d97af05db10858515cf10562a633b54678769
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-23 16:56:25 +0300

    add routs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import { useEffect } from "react";
-
+import { Route, Routes } from "react-router-dom";
+import { useTelegram } from "./hooks/useTelegram";
 import { Button } from "./components/Button/Button";
+import AccountList from "./components/AccountList/AccountList";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
 function App() {
+  const { tg, onClose } = useTelegram();
   useEffect(() => {
     tg.ready();
 
     console.log(tg.initData);
-  }, []);
+  }, [tg]);
 
   const onCloseButtonHandler = () => {
-    tg.close();
+    onClose();
   };
 
   return (
     <div className="app">
       <Button onClick={onCloseButtonHandler}>Close</Button>
+
+      <Routes>
+        <Route index element={<AccountList />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/AccountList/AccountList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AccountList(props) {
+    return (
+        <div>
+            Account List
+        </div>
+    );
+}
+
+export default AccountList;
