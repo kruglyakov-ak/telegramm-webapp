35febed182d85c3b504826469d04503476c29cc2
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-24 17:26:33 +0300

    add input styles

--- src/components/CreateAccountForm/CreateAccountForm.jsx
+++ src/components/CreateAccountForm/CreateAccountForm.jsx
@@ -5,19 +5,34 @@ import Input from "../Input/Input";
 import { AppRouterPath } from "../../constants";
 import "./CreateAccountForm.css";
 const CreateAccountForm = () => {
+  const [accountName, setAccountName] = React.useState("");
+  const [exchange, setExchange] = React.useState("");
+  const [mainApiKey, setMainApiKey] = React.useState("");
+  const [secondApiKey, setSecondApiKey] = React.useState("");
+
   return (
     <form className="create-account-form">
       <Input
         title={"Название аккаунта:"}
         type="text"
         placeholder="Название аккаунта"
+        value={accountName}
+        onChange={(e) => setAccountName(e.target.value)}
       />
       <Input title={"Выбор биржи:"} type="text" placeholder="Выбор биржи" />
-      <Input title={"Main API KEY:"} type="text" placeholder="Main API KEY" />
+      <Input
+        title={"Main API KEY:"}
+        value={mainApiKey}
+        onChange={(e) => setMainApiKey(e.target.value)}
+        type="text"
+        placeholder="Main API KEY"
+      />
       <Input
         title={"SECOND API KEY:"}
         type="text"
         placeholder="SECOND API KEY"
+        value={secondApiKey}
+        onChange={(e) => setSecondApiKey(e.target.value)}
       />
 
       <Button>Сохранить</Button>
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Input.css";
 
-const Input = ({ title, wrapperClassName = "", placeholder = "", ...props }) => {
-  return (
+const Input = ({ title, wrapperClassName = "", placeholder = "", value = "", ...props }) => {
+
+    return (
     <div className={`input-wrapper ${wrapperClassName}`}>
-      <input className="input" {...props} />
+      <input className={`input ${value !== "" ? "non-empty" : ""}`} {...props} />
       <div className="input-placeholder">{placeholder}</div>
     </div>
   );
