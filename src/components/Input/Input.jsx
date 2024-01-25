27750771ffb7e4c9d8d1ccd6a2e8a380ec76cef3
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-25 16:17:27 +0300

    add input class name

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Input.css";
 
-const Input = ({ title, wrapperClassName = "", placeholder = "", value = "", ...props }) => {
-
-    return (
+const Input = ({
+  title,
+  wrapperClassName = "",
+  placeholder = "",
+  className = "",
+  value = "",
+  ...props
+}) => {
+  return (
     <div className={`input-wrapper ${wrapperClassName}`}>
-      <input className={`input ${value !== "" ? "non-empty" : ""}`} {...props} />
+      <input
+        className={`input ${value !== "" ? "non-empty" : ""} ${className}`}
+        {...props}
+      />
       <div className="input-placeholder">{placeholder}</div>
     </div>
   );
