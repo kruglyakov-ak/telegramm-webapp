087fb4851bb35c09481b223891e311aa67097ea0
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-25 14:21:29 +0300

    add get account

--- src/components/AccountList/AccountList.jsx
+++ src/components/AccountList/AccountList.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "../Link/Link";
 import Button from "../Button/Button";
 import { AppRouterPath } from "../../constants";
+import { useTelegram } from "../../hooks/useTelegram";
 
 import "./AccountList.css";
 
 const accounts = [0, 1, 2, 3, 4, 5, 6, 7];
 function AccountList(props) {
+  const { tg } = useTelegram();
+
+  const fetchAccounts = async () => {
+    try {
+      const accounts = await fetch("http://192.168.88.27:8080/accounts/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Web-App-Init-Data": tg.initData,
+        },
+      });
+      return accounts;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAccounts();
+  });
+
   return (
     <div className="account-list">
       <Link to={AppRouterPath.CreateAccountForm}>
