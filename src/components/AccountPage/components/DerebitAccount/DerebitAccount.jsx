6ee5dc1d42f1b097ad4e0663b99a21709523964b
Aleksandr <aleksandr.k.kruglyakov@yandex.ru>
2024-01-29 18:14:03 +0300

    add get posible pairs

--- src/components/AccountPage/components/DerebitAccount/DerebitAccount.jsx
+++ src/components/AccountPage/components/DerebitAccount/DerebitAccount.jsx
@@ -1,18 +1,49 @@
 import React from "react";
+import { useTelegram } from "../../../../hooks/useTelegram";
 import BuyingFutures from "../BuyingFutures/BuyingFutures";
 import "./DerebitAccount.css";
 
-const options = [
-  { value: "BTCUSD_PERP", label: "BTCUSD_PERP" },
-];
-
 const DerebitAccount = () => {
+  const { tg } = useTelegram();
+  const [buyingOptions, setBuyingOptions] = React.useState([]);
+
+  const getPossiblePairs = React.useCallback(async () => {
+    try {
+      if (tg?.initData) {
+        const res = await fetch(
+          `https://transfer.meraquant.com/instruments/futures/pairs`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: tg?.initData,
+            },
+          }
+        );
+        const resData = await res.json();
+
+        setBuyingOptions(
+          resData?.data?.deribit?.buy.map(({ instrument_to }) => ({
+            value: instrument_to,
+            label: instrument_to,
+          }))
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [tg]);
+
+  React.useEffect(() => {
+    getPossiblePairs();
+  }, [getPossiblePairs, tg]);
+
   return (
     <div className="derebit-account">
       <div className="account-info"></div>
 
       <div className="actions-title">Покупка фьючерса</div>
-      <BuyingFutures currencyOptions={options} />
+      <BuyingFutures currencyOptions={buyingOptions} />
     </div>
   );
 };
