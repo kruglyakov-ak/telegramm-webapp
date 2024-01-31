a2850a03a429935259d8a28329dcd51c730d7b5f
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2024-01-31 16:56:29 +0300

    add styles

--- src/components/AccountList/AccountList.jsx
+++ src/components/AccountList/AccountList.jsx
@@ -44,34 +44,22 @@ function AccountList(props) {
         accounts.map((account) => (
           <Link key={account.id} to={AppRouterPath.Account(account.id)}>
             <Button key={account} className="account-button">
-              <div
-                className="account-button-inner"
-                style={{
-                  width:
-                    accounts.sort(function (a, b) {
-                      return b.title.length - a.title.length;
-                    })[0].title.length *
-                      10 +
-                    "px",
-                }}
-              >
-                {account.title}
-                <div className="exchange-logo-wrapper">
-                  {account.market === "binance" ? (
-                    <img
-                      className="exchange-logo"
-                      src="/images/binance-logo.svg"
-                      alt="Binance"
-                    />
-                  ) : (
-                    <img
-                      className="exchange-logo"
-                      src="/images/deribit-logo.svg"
-                      alt="Deribit"
-                      style={{ marginLeft: "2px" }}
-                    />
-                  )}
-                </div>
+              {account.title}
+              <div className="exchange-logo-wrapper">
+                {account.market === "binance" ? (
+                  <img
+                    className="exchange-logo"
+                    src="/images/binance-logo.svg"
+                    alt="Binance"
+                  />
+                ) : (
+                  <img
+                    className="exchange-logo"
+                    src="/images/deribit-logo.svg"
+                    alt="Deribit"
+                    style={{ marginLeft: "2px" }}
+                  />
+                )}
               </div>
             </Button>
           </Link>
